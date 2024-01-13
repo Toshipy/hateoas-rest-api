0b62cb97eed5723b01807b119605311a446b0b40
arai <arai@tagbangers.co.jp>
2024-01-13 15:21:59 +0900

    first commit

---
+++ src/test/java/springboot/hateoasrestapi/HateoasRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.hateoasrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
